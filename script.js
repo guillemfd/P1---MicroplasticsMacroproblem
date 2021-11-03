d4e3bc7ef4c36cf1e93ee4c560733eaf27ba4483
Guillem Ferrer <guillemfd@gmail.com>
2021-11-03 16:53:10 +0100

    Everything working

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const canvas = document.getElementById('main')
-const ctx = canvas.getContext('2d')
-canvas.width = 1000;
-canvas.height = 600;
+
+let ctx;
+if(canvas !== null){
+  ctx = canvas.getContext('2d')
+  canvas.width = 1000;
+  canvas.height = 600;
+}
+ 
+
+
+
+let score = 0
 
 
 
@@ -9,7 +18,9 @@ const loadedImages = {}
 
 const imageLinks = [ //Array de objetos con los enlaces (y los nombres para identificarlos) de todas mis imagenes
   {link: "../img/peix_transp_dreta.png", name: 'fish_right'},
+  {link: "../img/peix_transp_esquerra.png", name: 'fish_left'},
   {link: "../img/bubble.png", name: 'bubble'},
+  {link: "../img/sand_BG.png", name: 'sorra'},
 ]
 
 let counterForLoadedImages = 0;
@@ -24,19 +35,6 @@ imageLinks.forEach((imagen)=>{
     }
   }
 })
-
-
-// const pumpkinButton = document.getElementById('pumpkin')
-// const zenitsuButton = document.getElementById('zenitsu')
-
-// pumpkin.onclick = ()=>{
-//   ctx.drawImage(loadedImages.pumpkin, 200, 100, 100, 130)
-// }
-
-// zenitsu.onclick = ()=>{
-//   ctx.drawImage(loadedImages.zenitsu, 100, 50, 100, 130)
-// }
-
 
 
 
@@ -111,6 +109,7 @@ class Fish {
     this.speedY = 0;
     this.width = 100;
     this.height = 100;
+    this.direction = "left";
   }
 }
 
@@ -135,7 +134,7 @@ class Bubble {
   }
 
 
-  //---------------------------------------- COLISIÓN OPCIÓN JJ ------------
+  //---------------------------------------- COLISIÓN ------------
   checkForEatenBubbles(){
    const bothInX = (this.x - 90) < fish.x && this.x +50 > fish.x
    const bothInY = (this.y - 70) < fish.y && this.y > fish.y
@@ -144,7 +143,7 @@ class Bubble {
       this.eaten = true
       score++;
       bubbleSound.play()
-      // document.getElementById('score').innerText = score
+      document.getElementById('score').innerText = score
     }
   }
 }
@@ -155,13 +154,41 @@ let bubbleSound = new Audio('../sounds/bubble_sound.wav')
     bubbleSound.preload = 'auto'
     bubbleSound.load()
 
-let soundtrack = new Audio('../sounds/soundtrack.wav')
-    soundtrack.volume = 1
-    soundtrack.preload = 'auto'
-    soundtrack.load()
+// let soundtrack = new Audio('../sounds/soundtrack.wav')
+//     soundtrack.volume = 1
+//     soundtrack.preload = 'auto'
+//     soundtrack.load()
+
+const audio = document.getElementById('audio');
+const playPauseBTN = document.getElementById('pPButton')
+
+function playPause(){
+  if(!audio.paused){
+    audio.pause();
+    playPauseBTN.style.backgroundImage = 'url(../sounds/sound_off.png)';
+  } else {
+    audio.play();
+    playPauseBTN.style.backgroundImage = 'url(../sounds/sound_on.png)';
+  }
+}
+
+// var count = 0;
+
+// function playPause(){
+//   if(count == 0) {
+//     count = 1;
+//     audio.play();
+//     playPauseBTN.innerHTML = "Pause $#9208;";
+//   } else {
+//     count = 0;
+//     audio.pause();
+//     playPauseBTN.innerHTML = "Play $#9658;";
+//   }
+// }
 
 
-//--- MUSIC JUAGO JJ -----------
+
+//--- MUSIC JUEGO JJ -----------
 
 // let soundtrack = false;
 // let musicActivated = true;
@@ -223,9 +250,6 @@ let soundtrack = new Audio('../sounds/soundtrack.wav')
 
 
 
-
-
-
 let bubblesArray = []
 
 const createBubbles = setInterval(()=>{
@@ -261,14 +285,14 @@ const deleteBubble = ()=>{
 
 
 
-
-
 //FISH MOVEMENTS
 document.addEventListener('keydown', (event)=>{
   if(event.key === "ArrowRight"){
     fish.speedX = 5
+    fish.direction = 'right'
   } else if(event.key === "ArrowLeft"){
     fish.speedX = -5
+    fish.direction = 'left'
   } else if(event.key === "ArrowUp"){
     fish.speedY = -5
   } else if(event.key === "ArrowDown"){
@@ -285,10 +309,36 @@ document.addEventListener('keyup', (event)=>{
   }
 })
 
+
 //FUNCTIONS
-const drawFish = ()=>{
+
+const drawRightFish = ()=>{
   ctx.drawImage(loadedImages.fish_right, fish.x, fish.y, fish.width, fish.height)
 }
+
+const drawLeftFish = ()=>{
+  ctx.drawImage(loadedImages.fish_left, fish.x, fish.y, fish.width, fish.height)
+}
+
+// drawFish(){
+//   if(event.key === "ArrowLeft"){
+//   ctx.drawImage(loadedImages.fish_left, fish.x, fish.y, fish.width, fish.height)
+//   }else{
+//   ctx.drawImage(loadedImages.fish_right, fish.x, fish.y, fish.width, fish.height)
+//   }
+// }
+
+// const drawFish = ()=>{
+//   if('ArrowLeft' === true){
+//   ctx.drawImage(loadedImages.fish_left, fish.x, fish.y, fish.width, fish.height)
+//   }else{
+//   ctx.drawImage(loadedImages.fish_right, fish.x, fish.y, fish.width, fish.height)
+//   }
+// }
+
+
+
+
 
 const checkIfInBounds = ()=>{
   if(fish.x > 900){
@@ -319,45 +369,63 @@ const clearCanvas = ()=>{
 }
 
 
-  // ------------------------------ TIME -----------------------------------------
-  let time = 5
-  const interval = setInterval(() => {
-    if(time > 0){
-      time --;
-    }else{
-      clearInterval(interval);
-      //alert(`You have been polluted! \nYou catch ${score} bubbles`)
-      window.location.href = `./polluted.html?score=${score}`
-      const queryString = window.location.search
-      // document.querySelector('.move').innerText=`You achived to catch ${score} bubbles!`
-    }
-  }, 1000)
+// ------------------------------ TIME -----------------------------------------
+let time = 10
+const interval = setInterval(() => {
+  if(time > 0){
+    time --;
+  }else{
+    clearInterval(interval);
+    // alert(`You have been polluted! \nYou catch ${score} bubbles`)
+    window.location.href = `./polluted.html?score=${score}`
+    // window.location.href = `./polluted.html`
+    //window.location.assign("./polluted.html");
+    // const queryString = window.location.search
 
-  //You achived to catch ${score} bubbles!
 
-  const drawtime = (number) => {
-    ctx.font = "25px Montserrat";
-    ctx.fillStyle = "#003eaa"
-    ctx.fillText(`Remaining life time :${number}`, 25, 30)
   }
-  // ------------------------------ TIME -----------------------------------------
+}, 1000)
 
 
 
-  // ------------------------------ Score -----------------------------------------
-  let score = 0
-  const drawscore = (number) =>{
-    ctx.font = "25 arial"
-    ctx.fillStyle = "blue"
-    ctx.fillText(`Eaten bubbles:${number}`, 350, 30)
-  }
-  // ------------------------------ Score -----------------------------------------
+//SCORE
+
+// showScores() {
+//   // show scores
+//    this.ctx.font = '25px Verdana';
+//    this.ctx.fillStyle = 'black';
+//    this.ctx.fillText('Score: ' + this.score, 20, 40);
+// },
+
+//  let scoreElement = document.getElementById("score")
+//  scoreElement.innerHTML = "Score: "+ this.score;
+
+
+//You achived to catch ${score} bubbles!
+
+const drawtime = (number) => {
+  ctx.font = "25px Montserrat";
+  ctx.fillStyle = "#960000"
+  ctx.fillText(`Remaining life time :${number}`, 25, 40)
+}
+// ------------------------------ TIME -----------------------------------------
+
+
+
+// ------------------------------ Score -----------------------------------------
+
+const drawscore = (number) =>{
+  ctx.font = "25 arial"
+  ctx.fillStyle = "#000096"
+  ctx.fillText(`Eaten bubbles:${number}`, 25, 70)
+}
+// ------------------------------ Score -----------------------------------------
 
 
 
 
 
-//BACKGROUND 
+//BACKGROUND MICROPLÁSTICOS
 function animate () {
   requestAnimationFrame(animate);
   
@@ -366,6 +434,15 @@ function animate () {
 }
 animate()
 inint()
+
+
+
+//BACKGROUND ARENA
+const drawSand = ()=>{
+  ctx.drawImage(loadedImages.sorra,  0, 210, canvas.width, 400)
+}
+
+
 
 
 //RUNNING GAME
@@ -379,10 +456,15 @@ const updateCanvas = ()=>{
     deleteBubble()
     checkForEatenBubbles()
 
-    soundtrack.play()
-
-    drawFish()
+    if (fish.direction == "left") {
+      drawLeftFish();
+    } else {
+      drawRightFish();
+    }
+    
+    // drawFish()
     drawBubbles()
+    drawSand()
 
     drawtime(time)
     drawscore(score)
@@ -393,3 +475,22 @@ const updateCanvas = ()=>{
   }
 }
 updateCanvas()
+
+
+// window.addEventListener('onhashchange', (event) => {
+
+
+
+// });
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   const scoreElement = document.querySelector('#score');
+//   alert();
+
+//   if(score){
+//     scoreElement.innerText = score
+//   }
+ 
+// });
+
